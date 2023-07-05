d735d7ba1b4edcdafa7ee37fa350de85b218b793
JadisHee <837850248@qq.com>
2023-07-05 11:14:55 +0800

    添加注释

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import paramiko
 
 def remove_authorized_key(hostname, username, key_to_remove):
+    # 创建SSH客户端实例
     client = paramiko.SSHClient()
+    # 设置自动添加主机密钥
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     try:
+        # 连接到主机
         client.connect(hostname, username=username)
+        # 执行命令，读取authorized_keys文件内容
         ssh_stdin, ssh_stdout, ssh_stderr = client.exec_command('cat ~/.ssh/authorized_keys')
         authorized_keys = ssh_stdout.read().decode('utf-8').split('\n')
         if key_to_remove in authorized_keys:
+            # 如果要删除的公钥存在于authorized_keys中，将其移除
             authorized_keys.remove(key_to_remove)
             updated_keys = '\n'.join(authorized_keys)
-
+            # 将更新后的authorized_keys内容写入文件
             ssh_stdin, ssh_stdout, ssh_stderr = client.exec_command(f'echo "{updated_keys}" > ~/.ssh/authorized_keys')
             if ssh_stdout.channel.recv_exit_status() == 0:
                 print(f"SSH key removed from {hostname}")
@@ -22,6 +27,7 @@ def remove_authorized_key(hostname, username, key_to_remove):
     except Exception as e:
         print(f"Error occurred while removing SSH key from {hostname}: {str(e)}")
     finally:
+        # 关闭SSH连接
         client.close()
 
 # 主机的连接信息
@@ -56,5 +62,7 @@ slaves = [
 
 # 取消主机与从机之间的互信关系
 for slave in slaves:
+    # 从主机中移除从机的公钥
     remove_authorized_key(host['hostname'], host['username'], slave['key_to_remove'])
+    # 从从机中移除主机的公钥
     remove_authorized_key(slave['hostname'], slave['username'], host['keys_to_remove'])
